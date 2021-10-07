fee4df56af6cdc11ce444fe1229ba484300448fc
Neha Sontakke <wasnik.neha@gmail.com>
2021-10-07 07:22:37 +0530

    added the localstorage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
 const App = () => {
+const firstRender = useRef(true);
 const [inputValue,setInputValue] = useState("");
 const [todos,setTodos] = useState([]);
 
@@ -11,26 +12,46 @@ const addTodo = (e) => {
 if(inputValue.trim() === "") return;
 
 
-  setTodos([...todos,{
+  setTodos([
+    ...todos,{
     text:inputValue,
-    id:uuidv4(),
+    id: uuidv4(),
   },
 ]);
 setInputValue("");
 };
 
+const removeTodo = (id) =>{
+  setTodos(todos.filter((todo)=>todo.id !== id));
+}
 
+useEffect(()=>{
+  if(firstRender.current){
+   firstRender.current = false;
+}else{
+  localStorage.setItem("Todo",JSON.stringify([...todos]));
+}
+  
+},[todos]);
+
+useEffect(()=>{
+   if(localStorage.getItem("Todo") !== null){
+    const newTodos = localStorage.getItem("Todo");
+    setTodos(JSON.parse([...todos,newTodos])); 
+  }
+},[])
   return (
     <div className="App">
      <div className="container">
        <form onSubmit={addTodo}>
-         <input type= "text" placeholder="Add tasks...." value={inputValue}
+         <input autoFocus type= "text" placeholder="Add tasks...." value={inputValue}
          onChange={(e)=> setInputValue(e.target.value)}/>
          <button tyype="submit">Add</button>
        </form>
        {todos.map((todo)=>(
          <div key={todo.id} className="todo">
            <p>{todo.text}</p>
+           <i onClick={()=>removeTodo(todo.id)}className="far fa-trash-alt"></i>
            </div>
        ))}
      </div>
